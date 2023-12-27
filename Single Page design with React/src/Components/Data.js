5a0402af577d0caf018e312dd9e3a38a1f635447
Inzamam Ul Islam Nirob <141902192+InzamamNirob997@users.noreply.github.com>
2023-12-27 21:11:09 +0600

    Add files via upload

---
+++ Single Page design with React/src/Components/Data.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <main className='Data container'>
+        <div className='content'>
+
+            <h1 >
+            YOUR FEET DESERVE THE BEST
+            </h1>
+
+            <p>
+                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+        
+
+        <div className="main_content">
+        <button className='first-btn' >Shop Now</button>
+        <button className='second-btn'>Category</button>
+
+        </div>
+
+
+
+         <div className='p-items'>
+         <p>Also availble ON</p>
+        
+
+
+        <div className='brandname'>
+            <img src="/images/flipkart.png" alt="" />
+            <img src="/images/amazon.png" alt="" />
+
+        </div>
+        </div>
+        </div>
+
+
+
+
+
+        <div className='image'>
+            <img src="/images/shoe_image.png" alt="" />
+        </div>
+      
+      
+    </main>
+  )
+}
+
+export default Data
